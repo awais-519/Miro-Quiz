0e29cbe9bdc91fd0204b885c01120ff5600c131c
Awais Ul Hassan <awaisul519@gmail.com>
2023-03-02 23:46:42 +0500

    App Config: Replaced nodemon with node

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const quizzesRoute = require('./Routes/quizzes');
 const bodyParser = require('body-parser');
 const connection = require('./Database/connection');
 const { PORT } = require('./config');
-
 const app = Express();
 
-app.listen(PORT, () => console.log(`SERVERS LISTENING ON PORT`));
+app.listen(5000, () => console.log(`SERVERS LISTENING ON PORT`));
 
 // Connect to the database
 connection.connect((err) => {
 	if (err) {
 		console.error('Error connecting to MySQL database: ', err);
 	} else {
-		console.log('Connected to MySQL database');
+		console.log('Connecteds to MySQL database');
 	}
 });
 
